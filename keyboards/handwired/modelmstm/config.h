932a38bf8956111c6aa7063de559f80c1e037c62
Markus Fritsche <fritsche.markus@gmail.com>
2021-11-23 09:34:43 +0000

    add via layout file

--- keyboards/handwired/modelmstm/config.h
+++ keyboards/handwired/modelmstm/config.h
@@ -28,8 +28,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 16
 
-// #define SD1_CTS_PIN B9
-
 #define MATRIX_ROW_PINS { A0, A1, A2, A3, A4, A5, A6, A7 }
 #define MATRIX_COL_PINS { B8, B7, B6, B5, B4, B3, A15, C13, C14, A10, C15, A8, B15, B14, B13, B12 }
 #define UNUSED_PINS
@@ -43,28 +41,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE    5
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-//#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-//#define LOCKING_RESYNC_ENABLE
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
-
 #define EARLY_INIT_PERFORM_BOOTLOADER_JUMP TRUE
-//#define STM32_HSECLK 8000000
--- keyboards/handwired/modelmstm/modelmstm.c
+++ keyboards/handwired/modelmstm/modelmstm.c
@@ -1,27 +1,6 @@
 #include "modelmstm.h"
 #include QMK_KEYBOARD_H
 
-void board_init(void) {
-    // B9 is configured as I2C1_SDA in the board file; that function must be
-    // disabled before using B7 as I2C1_SDA.
-/*    setPinInputHigh(B12);
-    setPinInputHigh(B13);
-    setPinInputHigh(B14);
-    setPinInputHigh(B15);
-    setPinInputHigh(A8);
-    setPinInputHigh(A9);
-    setPinInputHigh(A10);
-    setPinInputHigh(A11);
-    setPinInputHigh(A12);
-    setPinInputHigh(A15);
-    setPinInputHigh(B3);
-    setPinInputHigh(B4);
-    setPinInputHigh(B5);
-    setPinInputHigh(B6);
-    setPinInputHigh(B7);
-    setPinInputHigh(B8);*/
-}
-
 #ifdef MATRIX_HAS_GHOST
 __attribute__ ((weak))
 bool matrix_has_ghost_in_row(uint8_t row)
