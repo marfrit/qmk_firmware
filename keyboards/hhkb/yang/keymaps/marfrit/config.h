b4f8355086aa90b25e4bf86773d012594b48b4a4
Markus Fritsche <fritsche.markus@gmail.com>
2022-06-20 19:01:24 +0000

    create my hhkb keymap

---
+++ keyboards/hhkb/yang/keymaps/marfrit/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// place overrides here
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#undef  TAPPING_TERM
+#define TAPPING_TERM 175
+
+#define UNICODE_SELECTED_MODES UC_LNX, UC_MAC, UC_WIN
