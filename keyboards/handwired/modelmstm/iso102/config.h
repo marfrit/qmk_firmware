3bbfe25bafbbde3191a2045bb52fdacc1fff7660
Markus Fritsche <fritsche.markus@gmail.com>
2022-07-23 15:57:08 +0000

    testing 102 fw

--- keyboards/handwired/modelmstm/iso102/config.h
+++ keyboards/handwired/modelmstm/iso102/config.h
@@ -31,6 +31,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define SD1_CTS_PIN B9
 
 #define MATRIX_ROW_PINS { A0, A1, A2, A3, A4, A5, A6, A7 }
+//#define MATRIX_ROW_PINS { A7, A6, A5, A4, A3, A2, A1, A0 }
+//#define MATRIX_COL_PINS { B9, B8, B7, B6, B5, B4, B3, A15, C13, C14, A10, C15, A8, B15, B14, B13 }
 #define MATRIX_COL_PINS { B9, B8, B7, B6, B5, B4, B3, A15, C13, C14, A10, C15, A8, B15, B14, B13 }
 #define UNUSED_PINS
 
--- keyboards/handwired/modelmstm/iso102/rules.mk
+++ keyboards/handwired/modelmstm/iso102/rules.mk
@@ -30,7 +30,6 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 LTO_ENABLE = no
 #EEPROM_DRIVER = transient
-HAS_LEDS = yes
-
+DEBUG_ENABLE = yes
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 
