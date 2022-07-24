a3970fb4f87b9c0381873b010cd1e2e8f8356292
Markus Fritsche <fritsche.markus@gmail.com>
2022-07-24 23:21:26 +0000

    add delete to convinient place

--- keyboards/gboards/gergo/keymaps/marfrit/keymap.c
+++ keyboards/gboards/gergo/keymaps/marfrit/keymap.c
@@ -83,13 +83,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------'                     `--------------'
  */
 [_RAISE] = LAYOUT_gergo(
-    KC_TRNS,  KC_ESC,  KC_7, KC_8, KC_9, KC_TRNS,                                          KC_HOME, KC_PGUP, KC_UP,   KC_PGDN, KC_BSPC, KC_TRNS,
+    KC_TRNS,  KC_ESC,  KC_7, KC_8, KC_9, KC_TRNS,                                          KC_HOME, KC_PGUP, KC_UP,   KC_PGDN, KC_BSPC, KC_DEL,
     KC_TRNS,  KC_TAB,  KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS,                       KC_TRNS,  KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT,  KC_TRNS,
     KC_TRNS,  KC_CAPS, KC_1, KC_2, KC_3, KC_0,    KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_UNDO, KC_CUT, KC_COPY,  KC_PSTE, KC_DEL,  KC_TRNS,
                               KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,     KC_TRNS
     ),
 [_ADJUST] = LAYOUT_gergo(
-    RESET,   KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
+    RESET,   KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     EEP_RST, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                         KC_TRNS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD, KC_VOLU,
     KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,     KC_TRNS, KC_TRNS,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_MPLY, KC_MNXT,
                                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS
