c7d8eab9d193e56652a7d7cdc319ef5e2786546b
Markus Fritsche <fritsche.markus@gmail.com>
2022-09-22 18:24:44 +0000

    minor keymap updates

--- keyboards/lily58/keymaps/marfritlily/keymap.c
+++ keyboards/lily58/keymaps/marfritlily/keymap.c
@@ -2,28 +2,42 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-        ALL_T(KC_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
-        KC_TAB, KC_K, KC_U, KC_Q, LT(3,KC_DOT), KC_J, KC_V, LT(3,KC_G), KC_C, KC_L, KC_F, KC_DEL,
-        KC_LSFT, KC_H, LALT_T(KC_I), LCTL_T(KC_E), LSFT_T(KC_A), KC_O, KC_D, RSFT_T(KC_T), RCTL_T(KC_R), LALT_T(KC_N), KC_S, KC_ENT,
-        KC_LGUI, KC_X, RALT_T(KC_Y), KC_SCLN, LT(1,KC_COMM), LT(2,KC_SLSH), KC_LBRC, KC_RBRC, LT(2,KC_B), LT(1,KC_P), KC_W, RALT_T(KC_M), KC_Z, KC_RSFT,
-        MO(3), MO(2), MO(1), KC_SPC, KC_BSPC, MO(1), MO(2), MO(3)),
+/*
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | BSPC |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   K  |   U  |   Q  |   .  |   J  |                    |   V  |   G  |   C  |   L  |   F  | DEL  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |LShift|   H  |   I  |   E  |   A  |   O  |-------.    ,-------|   D  |   T  |   R  |   N  |   S  |Enter |
+ * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
+ * |LGUI  |   X  |   Y  |   ;  |   ,  |   /  |-------|    |-------|   B  |   P  |   W  |   M  |   Z  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+*/
+        ALL_T(KC_ESC), KC_1, KC_2,         KC_3,         KC_4,          KC_5,                             KC_6,       KC_7,         KC_8,         KC_9,         KC_0, KC_BSPC,
+        KC_TAB,        KC_K, KC_U,         KC_Q,         LT(3,KC_DOT),  KC_J,                             KC_V,       LT(3,KC_G),   KC_C,         KC_L,         KC_F, KC_DEL,
+        KC_LSFT,       KC_H, LALT_T(KC_I), LCTL_T(KC_E), LSFT_T(KC_A),  KC_O,                             KC_D,       RSFT_T(KC_T), RCTL_T(KC_R), LALT_T(KC_N), KC_S, KC_ENT,
+        KC_LGUI,       KC_X, RALT_T(KC_Y), KC_SCLN,      LT(1,KC_COMM), LT(2,KC_SLSH), KC_LBRC,  KC_RBRC, LT(2,KC_B), LT(1,KC_P),   KC_W,         RALT_T(KC_M), KC_Z, KC_RSFT,
+                                           MO(3),        MO(2),         MO(1),         KC_SPC,   KC_BSPC, MO(1),      MO(2),        MO(3)),
 	[1] = LAYOUT(
-        KC_F11, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F12,
-        KC_TRNS, KC_AT, KC_UNDS, KC_LBRC, KC_RBRC, KC_CIRC, KC_EXLM, KC_LT, KC_GT, KC_EQL, KC_AMPR, KC_TRNS,
-        KC_TRNS, KC_BSLS, KC_SLSH, KC_LCBR, KC_RCBR, KC_ASTR, KC_QUES, KC_LPRN, KC_RPRN, KC_MINS, KC_COLN, KC_TRNS,
-        KC_TRNS, KC_HASH, KC_DLR, KC_PIPE, KC_TILD, KC_GRV, KC_TRNS, KC_TRNS, KC_PLUS, KC_PERC, KC_DQUO, KC_QUOT, KC_SCLN, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(3), KC_TRNS, KC_TRNS),
+        KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
+        KC_TRNS, KC_AT,   KC_UNDS, KC_LBRC, KC_RBRC, KC_CIRC,                    KC_EXLM, KC_LT,   KC_GT,   KC_EQL,  KC_AMPR, KC_TRNS,
+        KC_TRNS, KC_BSLS, KC_SLSH, KC_LCBR, KC_RCBR, KC_ASTR,                    KC_QUES, KC_LPRN, KC_RPRN, KC_MINS, KC_COLN, KC_TRNS,
+        KC_TRNS, KC_HASH, KC_DLR,  KC_PIPE, KC_TILD, KC_GRV,  KC_TRNS,  KC_TRNS, KC_PLUS, KC_PERC, KC_DQUO, KC_QUOT, KC_SCLN, KC_TRNS,
+                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, MO(3), KC_TRNS, KC_TRNS),
 	[2] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_ESC, KC_7, KC_8, KC_9, KC_TRNS, KC_HOME, KC_PGUP, KC_UP, KC_PGDN, KC_BSPC, KC_TRNS,
-        KC_TRNS, KC_TAB, KC_4, KC_5, KC_6, KC_TRNS, KC_END, KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT, KC_TRNS,
-        KC_TRNS, KC_CAPS, KC_1, KC_2, KC_3, KC_0, KC_TRNS, KC_TRNS, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, KC_DEL, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_ESC,  KC_7,    KC_8,    KC_9,    KC_TRNS,                    KC_HOME, KC_PGUP, KC_UP,   KC_PGDN, KC_BSPC, KC_TRNS,
+        KC_TRNS, KC_TAB,  KC_4,    KC_5,    KC_6,    KC_TRNS,                    KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT,  KC_TRNS,
+        KC_TRNS, KC_CAPS, KC_1,    KC_2,    KC_3,    KC_0,    KC_TRNS,  KC_TRNS, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, KC_DEL,  KC_TRNS,
+                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 	[3] = LAYOUT(
-        KC_TRNS, UC_M_LN, UC_M_MA,  UC_M_WI,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,
-        KC_TRNS, KC_TRNS, RALT(KC_Y),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, RGB_VAI, RGB_MOD, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, RALT(KC_5), RALT(KC_Q), RALT(KC_P), KC_TRNS, BL_OFF, RGB_VAD, RGB_RMOD, RALT(KC_S), KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,    QK_BOOT,
+        KC_TRNS, KC_TRNS, RALT(KC_Y), KC_TRNS,    KC_TRNS,    KC_TRNS,                       KC_TRNS, RGB_TOG, RGB_VAI, RGB_MOD,  KC_TRNS,    KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,    RALT(KC_5), RALT(KC_Q), RALT(KC_P),                    KC_TRNS, BL_OFF,  RGB_VAD, RGB_RMOD, RALT(KC_S), KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,
+                                      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
--- keyboards/lily58/keymaps/marfritlily/rules.mk
+++ keyboards/lily58/keymaps/marfritlily/rules.mk
@@ -1,21 +1,15 @@
 AUTO_SHIFT_ENABLE = no
-#TAP_DANCE_ENABLE = yes
 LTO_ENABLE = yes
-BOOTMAGIC_ENABLE = no        # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no         # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes        # Audio control and System control(+450)
-#CONSOLE_ENABLE = no          # Console for debug(+400)
-#COMMAND_ENABLE = no          # Commands for debug and configuration
-#NKRO_ENABLE = no             # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-#BACKLIGHT_ENABLE = yes        # Enable keyboard backlight functionality
+NKRO_ENABLE = no
 MIDI_ENABLE = no             # MIDI controls
 AUDIO_ENABLE = no            # Audio output on port C6
 BLUETOOTH_ENABLE = no        # Enable Bluetooth with the Adafruit EZ-Key HID
-#RGBLIGHT_ENABLE = no         # Enable WS2812 RGB underlight.
-#API_SYSEX_ENABLE = no
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_DRIVER_ENABLE= yes     # OLED display
+OLED_ENABLE = yes
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = yes    # Breathing sleep LED during USB suspend
