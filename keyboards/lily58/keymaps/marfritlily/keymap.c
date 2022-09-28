e7acc05a275e5dbddf3728ede9eb74fdc4e29d59
Markus Fritsche <mfritsche@reauktion.de>
2022-09-28 08:59:49 +0200

    add skype and teams push to talk

--- keyboards/lily58/keymaps/marfritlily/keymap.c
+++ keyboards/lily58/keymaps/marfritlily/keymap.c
@@ -1,5 +1,12 @@
 #include QMK_KEYBOARD_H
 
+#define MUTE_HOLD_DELAY 400
+
+enum custom_keycodes {
+    MUTE_TEAMS = SAFE_RANGE,
+    MUTE_SKYPE,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
 /*
@@ -16,11 +23,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
 */
-        ALL_T(KC_ESC), KC_1, KC_2,         KC_3,         KC_4,          KC_5,                             KC_6,       KC_7,         KC_8,         KC_9,         KC_0, KC_BSPC,
-        KC_TAB,        KC_K, KC_U,         KC_Q,         LT(3,KC_DOT),  KC_J,                             KC_V,       LT(3,KC_G),   KC_C,         KC_L,         KC_F, KC_DEL,
-        KC_LSFT,       KC_H, LALT_T(KC_I), LCTL_T(KC_E), LSFT_T(KC_A),  KC_O,                             KC_D,       RSFT_T(KC_T), RCTL_T(KC_R), LALT_T(KC_N), KC_S, KC_ENT,
-        KC_LGUI,       KC_X, RALT_T(KC_Y), KC_SCLN,      LT(1,KC_COMM), LT(2,KC_SLSH), KC_LBRC,  KC_RBRC, LT(2,KC_B), LT(1,KC_P),   KC_W,         RALT_T(KC_M), KC_Z, KC_RSFT,
-                                           MO(3),        MO(2),         MO(1),         KC_SPC,   KC_BSPC, MO(1),      MO(2),        MO(3)),
+        ALL_T(KC_ESC), KC_1, KC_2,         KC_3,         KC_4,          KC_5,                                  KC_6,       KC_7,         KC_8,         KC_9,         KC_0, KC_BSPC,
+        KC_TAB,        KC_K, KC_U,         KC_Q,         LT(3,KC_DOT),  KC_J,                                  KC_V,       LT(3,KC_G),   KC_C,         KC_L,         KC_F, KC_DEL,
+        KC_LSFT,       KC_H, LALT_T(KC_I), LCTL_T(KC_E), LSFT_T(KC_A),  KC_O,                                  KC_D,       RSFT_T(KC_T), RCTL_T(KC_R), LALT_T(KC_N), KC_S, KC_ENT,
+        KC_LGUI,       KC_X, RALT_T(KC_Y), KC_SCLN,      LT(1,KC_COMM), LT(2,KC_SLSH), MUTE_TEAMS, MUTE_SKYPE, LT(2,KC_B), LT(1,KC_P),   KC_W,         RALT_T(KC_M), KC_Z, KC_RSFT,
+                                           MO(3),        MO(2),         MO(1),         KC_SPC,     KC_BSPC,    MO(1),      MO(2),        MO(3)),
 	[1] = LAYOUT(
         KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
         KC_TRNS, KC_AT,   KC_UNDS, KC_LBRC, KC_RBRC, KC_CIRC,                    KC_EXLM, KC_LT,   KC_GT,   KC_EQL,  KC_AMPR, KC_TRNS,
@@ -41,3 +48,75 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
+#ifdef OLED_ENABLE
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+  if (!is_keyboard_master())
+    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+  return rotation;
+}
+
+// When you add source files to SRC in rules.mk, you can use functions.
+const char *read_layer_state(void);
+const char *read_logo(void);
+void set_keylog(uint16_t keycode, keyrecord_t *record);
+const char *read_keylog(void);
+const char *read_keylogs(void);
+
+// const char *read_mode_icon(bool swap);
+// const char *read_host_led_state(void);
+// void set_timelog(void);
+// const char *read_timelog(void);
+
+bool oled_task_user(void) {
+  if (is_keyboard_master()) {
+    // If you want to change the display of OLED, you need to change here
+    oled_write_ln(read_layer_state(), false);
+    oled_write_ln(read_keylog(), false);
+    oled_write_ln(read_keylogs(), false);
+    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
+    //oled_write_ln(read_host_led_state(), false);
+    //oled_write_ln(read_timelog(), false);
+  } else {
+    oled_write(read_logo(), false);
+  }
+    return false;
+}
+#endif // OLED_ENABLE
+
+static uint16_t mute_hold_timert = 0;
+static uint16_t mute_hold_timers = 0;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+#ifdef OLED_ENABLE
+    set_keylog(keycode & 255, record);  // Ignore LT et al
+#endif
+    // set_timelog();
+  }
+  switch (keycode)
+  {
+  case MUTE_TEAMS:
+    if (record->event.pressed) {
+        mute_hold_timert = timer_read();
+        tap_code16(LCTL(LSFT(KC_M)));
+    } else {
+        if (timer_elapsed(mute_hold_timert) > MUTE_HOLD_DELAY)
+            tap_code16(LCTL(LSFT(KC_M)));
+    }
+    break;
+  case MUTE_SKYPE:
+    if (record->event.pressed) {
+        mute_hold_timers = timer_read();
+        tap_code16(LGUI(KC_F4));
+    } else {
+        if (timer_elapsed(mute_hold_timers) > MUTE_HOLD_DELAY)
+            tap_code16(LGUI(KC_F4));
+    }
+    break;
+
+  default:
+    break;
+  }
+  return true;
+}
