f5662c3b1892f35d48aed8676c7dd91b704268bc
Markus Fritsche <fritsche.markus@gmail.com>
2023-03-29 13:46:22 +0000

    update gergo to new kc names

--- keyboards/gboards/gergo/keymaps/marfrit/keymap.c
+++ keyboards/gboards/gergo/keymaps/marfrit/keymap.c
@@ -6,7 +6,7 @@
  * There's also a template for adding new layers at the bottom of this file!
  */
 
-#include QMK_KEYBOARD_H
+#include "marfrit.h"
 
 #define _BASE   0 // default layer
 #define _LOWER  1 // symbols
@@ -89,8 +89,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                               KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,     KC_TRNS
     ),
 [_ADJUST] = LAYOUT_gergo(
-    RESET,   KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    EEP_RST, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                         KC_TRNS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD, KC_VOLU,
+    QK_BOOT, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+    EE_CLR,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                         KC_TRNS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD, KC_VOLU,
     KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,     KC_TRNS, KC_TRNS,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_MPLY, KC_MNXT,
                                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS
     ),
